153e79e46d89e9831da5c7e9925ad58da6e0f2ba
Luis Alejo <lou.m.alejo@gmail.com>
2020-06-21 20:46:16 -0400

    updated names

--- src/Containers/HomeContainer.js
+++ src/Containers/HomeContainer.js
@@ -119,11 +119,11 @@ const HomeContainer = () => {
 			</div>
 
 			<div className="footer-info-container">
-				<span className="footer-info">© COPYRIGHT 2020 MATTEO DESANTIS, LLC</span>
+				<span className="footer-info">© COPYRIGHT 2020 MATTHEW DESANTIS, LLC</span>
 				<span className="footer-info">CONTACT:</span>
 				<span className="footer-info">908.723.5320</span>
 				<span className="footer-info">
-					<a className="footer-links" href="mailto:matteodesantis@gmail.com" target="_blank">
+					<a className="footer-links" href="mailto:matthewdesantis@gmail.com" target="_blank">
 						MatteoDeSantis@gmail.com
 					</a>
 				</span>
