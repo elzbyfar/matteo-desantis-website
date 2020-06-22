c0e1f739ed98d1c894cde176bdce3596140e8de8
Luis Alejo <lou.m.alejo@gmail.com>
2020-06-21 20:44:25 -0400

    bio draft

--- src/AssetLibraries/Images.js
+++ src/AssetLibraries/Images.js
@@ -1,6 +1,9 @@
 const Images = {
   backgroundVideo: require('../Images/background-vid.gif'), 
-  logo: require('../Images/matteo-logo.png')
+  logo: require('../Images/matteo-logo.png'),
+  matthew1: require('../Images/matthew01.jpg'), 
+  matthew2: require('../Images/matthew02.jpg')
+
 }
 
 export default Images
--- src/Containers/HomeContainer.js
+++ src/Containers/HomeContainer.js
@@ -11,18 +11,79 @@ const HomeContainer = () => {
 		<div className="home-container-container">
 			<div className="home-body">
 				<img src={Images.logo} className={about || book || catalogue ? 'small-logo' : 'logo'} />
-				<div className={about || book || catalogue ? 'content' : 'content-closed'} />
-        
-        {/* <div className="about-content-container">
-          <div className="content-links">
-            <ul>
-              <li>BIO</li>
-              <li>GALLERY</li>
-              <li>PRESS + TESTIMONIALS</li>
-            </ul>
-          </div>
-          <div className="about-content"></div>
-        </div> */}
+				<div className={about || book || catalogue ? 'content' : 'content-closed'}>
+					<div className="about-content-container">
+						<div className="content-links">
+							<button className="sub-links">BIO</button>
+							<button className="sub-links">GALLERY</button>
+							<button className="sub-links">PRESS</button>
+							<button className="sub-links">TESTIMONIALS</button>
+						</div>
+
+						<div className="about-content">
+							<span className="content-bio">
+								<img className="bio-photo-1" src={Images.matthew1} />
+								<p>
+									Matthew DeSantis is an exciting and upcoming entertainer based in the New York City
+									and Miami-Dade area. He performs with unique enthusiasm spurring a fun-loving
+									atmosphere that transmits clearly to his audiences. He has performed as an opening
+									act for major bands such as Dwight Yoakum, Steve Moakler, Blues Traveler and Lone
+									Star. Powered by his one man band machine; he has traveled the world bringing his
+									music to both the private and public arenas in the Tri-state area and in Miami
+									Florida. Check him out on Instagram @MattyDMusic.
+								</p>
+								<p>
+									Matthew DeSantis is an exciting and upcoming entertainer based in the New York City
+									and Miami-Dade area. He performs with unique enthusiasm spurring a fun-loving
+									atmosphere that transmits clearly to his audiences. He has performed as an opening
+									act for major bands such as Dwight Yoakum, Steve Moakler, Blues Traveler and Lone
+									Star. Powered by his one man band machine; he has traveled the world bringing his
+									music to both the private and public arenas in the Tri-state area and in Miami
+									Florida. Check him out on Instagram @MattyDMusic.
+								</p>
+								<p>
+									Matthew DeSantis is an exciting and upcoming entertainer based in the New York City
+									and Miami-Dade area. He performs with unique enthusiasm spurring a fun-loving
+									atmosphere that transmits clearly to his audiences. He has performed as an opening
+									act for major bands such as Dwight Yoakum, Steve Moakler, Blues Traveler and Lone
+									Star. Powered by his one man band machine; he has traveled the world bringing his
+									music to both the private and public arenas in the Tri-state area and in Miami
+									Florida. Check him out on Instagram @MattyDMusic.
+								</p>
+								<img className="bio-photo-2" src={Images.matthew2} />
+								
+								<p>
+									Matthew DeSantis is an exciting and upcoming entertainer based in the New York City
+									and Miami-Dade area. He performs with unique enthusiasm spurring a fun-loving
+									atmosphere that transmits clearly to his audiences. He has performed as an opening
+									act for major bands such as Dwight Yoakum, Steve Moakler, Blues Traveler and Lone
+									Star. Powered by his one man band machine; he has traveled the world bringing his
+									music to both the private and public arenas in the Tri-state area and in Miami
+									Florida. Check him out on Instagram @MattyDMusic.
+								</p>
+								
+								<p>
+									Matthew DeSantis is an exciting and upcoming entertainer based in the New York City
+									and Miami-Dade area. He performs with unique enthusiasm spurring a fun-loving
+									atmosphere that transmits clearly to his audiences. He has performed as an opening
+									act for major bands such as Dwight Yoakum, Steve Moakler, Blues Traveler and Lone
+									Star. Powered by his one man band machine; he has traveled the world bringing his
+									music to both the private and public arenas in the Tri-state area and in Miami
+									Florida. Check him out on Instagram @MattyDMusic.
+								</p>
+								<p>
+									Matthew DeSantis is an exciting and upcoming entertainer based in the New York City
+									and Miami-Dade area. He performs with unique enthusiasm spurring a fun-loving
+									atmosphere that transmits clearly to his audiences. He has performed as an opening
+									act for major bands such as Dwight Yoakum, Steve Moakler, Blues Traveler and Lone
+									Star. Powered by his one man band machine; he has traveled the world bringing his
+									music to both the private and public arenas in the Tri-state area and in Miami
+									Florida. Check him out on Instagram @MattyDMusic.
+								</p>
+							</span>
+						</div>
+					</div>
+				</div>
 				<div className="buttons-container">
 					<button
 						onClick={() => {
@@ -58,15 +119,22 @@ const HomeContainer = () => {
 			</div>
 
 			<div className="footer-info-container">
-        
 				<span className="footer-info">© COPYRIGHT 2020 MATTEO DESANTIS, LLC</span>
 				<span className="footer-info">CONTACT:</span>
 				<span className="footer-info">908.723.5320</span>
-				<span className="footer-info"><a className="footer-links" href="mailto:matteodesantis@gmail.com" target="_blank">MatteoDeSantis@gmail.com</a></span>
-				<span className="footer-info">WEB DESIGN: <a className="footer-links" href="https://www.linkedin.com/in/alejoluis/" target="_blank">Luis Alejo</a></span>
-
+				<span className="footer-info">
+					<a className="footer-links" href="mailto:matteodesantis@gmail.com" target="_blank">
+						MatteoDeSantis@gmail.com
+					</a>
+				</span>
+				<span className="footer-info">
+					WEB DESIGN:{' '}
+					<a className="footer-links" href="https://www.linkedin.com/in/alejoluis/" target="_blank">
+						Luis Alejo
+					</a>
+				</span>
 			</div>
-			<img src={Images.backgroundVideo} className="background-video" />
+			{/* <img src={Images.backgroundVideo} className="background-video" /> */}
 		</div>
 	);
 };
