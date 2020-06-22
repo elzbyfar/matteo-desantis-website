44f80736ede4534785a1cbf55dcc4457a34b8817
Luis Alejo <lou.m.alejo@gmail.com>
2020-06-22 01:04:56 -0400

    added public_url prefix

---
+++ docs/precache-manifest.1c1de4fd4bf60fc7b02ae357eb8d61c3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cbdc4454f116aa65a5907f70c5ef3c0",
+    "url": "/matteo-desantis-website/index.html"
+  },
+  {
+    "revision": "f531a55dce2555ca0ca8",
+    "url": "/matteo-desantis-website/static/css/main.a451150b.chunk.css"
+  },
+  {
+    "revision": "4bf86065c4a54f235555",
+    "url": "/matteo-desantis-website/static/js/2.51c49895.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/matteo-desantis-website/static/js/2.51c49895.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f531a55dce2555ca0ca8",
+    "url": "/matteo-desantis-website/static/js/main.9ceb21d0.chunk.js"
+  },
+  {
+    "revision": "819774063979b98cd50c",
+    "url": "/matteo-desantis-website/static/js/runtime-main.308a4c76.js"
+  },
+  {
+    "revision": "8a19f9a6e647c021d41c003544a5bcc5",
+    "url": "/matteo-desantis-website/static/media/background-vid.8a19f9a6.gif"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/matteo-desantis-website/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "00da91e4f19712f08532b3f9a1134a87",
+    "url": "/matteo-desantis-website/static/media/matteo-logo.00da91e4.png"
+  },
+  {
+    "revision": "7119e6535dd541027a1a8695024bcaf4",
+    "url": "/matteo-desantis-website/static/media/matthew01.7119e653.jpg"
+  },
+  {
+    "revision": "d5d393c98de3b93048f3de37483b6f9d",
+    "url": "/matteo-desantis-website/static/media/matthew02.d5d393c9.jpg"
+  }
+]);
