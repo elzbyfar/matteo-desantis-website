811d7f2e866d6de4e45c6963c9e2a2e2e77f7927
Luis Alejo <lou.m.alejo@gmail.com>
2020-07-04 07:33:53 -0400

    what about now

--- src/Components/Gallery.js
+++ src/Components/Gallery.js
@@ -6,8 +6,8 @@ const Gallery = () => {
   return (
     <div className="gallery-container">
       <div className="image-card">
-        <div className="image-title">
-          <h4>MATTHEW 01</h4>
+        <div className="image-title-container">
+          <h4 className="image-title">MATTHEW DeSANTIS 01</h4>
         </div>
         <div className="photo-container">
           <img src={Images.matthew1} className="actual-photo"/>
