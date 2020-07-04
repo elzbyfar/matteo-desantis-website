849449c43b35c4295bf95b48a6ac69fd04c948f7
Luis Alejo <lou.m.alejo@gmail.com>
2020-07-04 07:16:09 -0400

    trying this again

--- src/Components/Gallery.js
+++ src/Components/Gallery.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import Images from '../AssetLibraries/Images'
 
+
 const Gallery = () => {
   return (
-    <div>
-      GALERIA
+    <div className="gallery-container">
+      <div className="image-card">
+        <div className="image-title">
+          <h4>MATTHEW 01</h4>
+        </div>
+        <div className="photo-container">
+          <img src={Images.matthew1} className="actual-photo"/>
+        </div>
+      </div>
     </div>
   );
 }
